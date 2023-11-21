57d24713b1d625411197a27dbfea87ab80c4c022
comsaziz <comsaziz2@gmail.com>
2023-11-21 04:23:32 +0300

    init

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 from sklearn.feature_extraction.text import TfidfVectorizer
 import pandas as pd
 import numpy as np
@@ -24,24 +25,31 @@ documents = [
 # Given dictionary
 dictionary = ["Application", "Vaccination", "Covid", "Pilgrims", "Health", "Certificate", "Center"]
 
+# Convert documents to lowercase
+documents_lower = [doc.lower() for doc in documents]
+
+# Convert dictionary to lowercase
+dictionary_lower = [word.lower() for word in dictionary]
+
+# Now use documents_lower and dictionary_lower in your TF-IDF calculations
 
 # Calculate TF values
-tf_matrix = np.zeros((len(documents), len(dictionary)))
-for i, doc in enumerate(documents):
-    for j, word in enumerate(dictionary):
+tf_matrix = np.zeros((len(documents_lower), len(dictionary_lower)))
+for i, doc in enumerate(documents_lower):
+    for j, word in enumerate(dictionary_lower):
         tf_matrix[i, j] = doc.count(word) / len(doc.split())
 
 # Display the TF values as a DataFrame
-tf_df = pd.DataFrame(data=tf_matrix, columns=dictionary)
+tf_df = pd.DataFrame(data=tf_matrix, columns=dictionary_lower)
 print("TF values:")
 print(tf_df)
 
 # Calculate IDF values
-total_documents = len(documents)
+total_documents = len(documents_lower)
 idf_vector = np.log2(total_documents / np.count_nonzero(tf_matrix, axis=0))
 
 # Display the IDF values as a DataFrame
-idf_df = pd.DataFrame(data={'IDF': idf_vector}, index=dictionary)
+idf_df = pd.DataFrame(data={'IDF': idf_vector}, index=dictionary_lower)
 print("\nIDF values:")
 print(idf_df)
 
@@ -49,6 +57,15 @@ print(idf_df)
 tfidf_matrix = tf_matrix * idf_vector
 
 # Display the TF-IDF values as a DataFrame
-tfidf_df = pd.DataFrame(data=tfidf_matrix, columns=dictionary)
+tfidf_df = pd.DataFrame(data=tfidf_matrix, columns=dictionary_lower)
 print("\nTF-IDF values:")
+print(tfidf_df)
+
+#---------------------------------------------------------------
+tfidf_norm = np.linalg.norm(tfidf_matrix, axis=1, keepdims=True)
+tfidf_matrix /= tfidf_norm
+
+# Display the normalized TF-IDF values as a DataFrame
+tfidf_df = pd.DataFrame(data=tfidf_matrix, columns=dictionary)
+print("\nNormalized TF-IDF values:")
 print(tfidf_df)
