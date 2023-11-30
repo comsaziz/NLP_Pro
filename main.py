2cfca73e23cfffcb6e73e56719857dd36c57cc86
comsaziz <comsaziz2@gmail.com>
2023-11-30 16:16:29 +0300

    init

--- main.py
+++ main.py
@@ -1,17 +1,7 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 import pandas as pd
 import numpy as np
-
-# # Define the documents
-# documents = [
-#     "Data Base System Concepts",
-#     "Introduction to Algorithms",
-#     "Computer Geometry Application",
-#     "Data Structure and Algorithm Analysis on Data"
-# ]
-
-# # Define the dictionary (7 words)
-# dictionary = ["Data", "System", "Algorithm", "Computer", "Geometry", "Structure", "Analysis"]
+from sklearn.metrics.pairwise import cosine_similarity
 
 # Given documents
 documents = [
@@ -29,8 +19,6 @@ documents_lower = [doc.lower() for doc in documents]
 
 # Convert dictionary to lowercase
 dictionary_lower = [word.lower() for word in dictionary]
-
-# Now use documents_lower and dictionary_lower in your TF-IDF calculations
 
 # Calculate TF values
 tf_matrix = np.zeros((len(documents_lower), len(dictionary_lower)))
@@ -60,11 +48,48 @@ tfidf_df = pd.DataFrame(data=tfidf_matrix, columns=dictionary_lower)
 print("\nTF-IDF values:")
 print(tfidf_df)
 
-#---------------------------------------------------------------
+# Normalize TF-IDF values
 tfidf_norm = np.linalg.norm(tfidf_matrix, axis=1, keepdims=True)
 tfidf_matrix /= tfidf_norm
 
 # Display the normalized TF-IDF values as a DataFrame
-tfidf_df = pd.DataFrame(data=tfidf_matrix, columns=dictionary)
+tfidf_df_normalized = pd.DataFrame(data=tfidf_matrix, columns=dictionary_lower)
 print("\nNormalized TF-IDF values:")
-print(tfidf_df)
+print(tfidf_df_normalized)
+
+
+
+
+#------Start of Ranking code section
+# Query
+query = "Covid Vaccination"
+
+# Convert the query to lowercase
+query_lower = query.lower()
+
+# Calculate TF values for the query
+query_tf = np.zeros((1, len(dictionary_lower)))
+for j, word in enumerate(dictionary_lower):
+    query_tf[0, j] = query_lower.count(word) / len(query_lower.split())
+
+# Calculate IDF values for the query
+query_idf = np.log2(total_documents / np.count_nonzero(tf_matrix, axis=0))
+
+# Calculate TF-IDF values for the query
+query_tfidf = query_tf * query_idf
+
+# Normalize the TF-IDF values for the query
+query_tfidf_norm = np.linalg.norm(query_tfidf)
+
+# Calculate Cosine Similarity scores
+cosine_sim_scores = cosine_similarity(tfidf_matrix, query_tfidf)[:, 0]
+
+# Create a DataFrame to display the results
+result_df = pd.DataFrame(data={'Document': documents, 'Cosine Similarity Score': cosine_sim_scores})
+
+# Rank the documents based on the scores
+result_df['Rank'] = result_df['Cosine Similarity Score'].rank(ascending=False)
+
+# Display the ranked results
+print("\nRanked Results for the Query 'Covid Vaccination':")
+print(result_df.sort_values(by='Rank'))
